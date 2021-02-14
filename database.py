1f507cb32b43cd91d46106fe1d0b7e98dd67c9a9
kidflash2jahaan <53150988+kidflash2jahaan@users.noreply.github.com>
2021-02-14 09:21:58 -0800

    1.1
    database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import sqlite3
 
 def connect_db():
     sql = sqlite3.connect(
-        "/Users/jahaan/OneDrive/Programming/webapp/Forum/questions.db")
+        "./questions.db")
     print("True")
     sql.row_factory = sqlite3.Row
     return sql
@@ -13,4 +13,5 @@ def connect_db():
 def get_db():
     if not hasattr(g, "sqlite_db"):
         g.sqlite_db = connect_db()
+        
     return g.sqlite_db
