3a455bc661064bb8a88e5a1fcd5072a710beac67
kidflash2jahaan <53150988+kidflash2jahaan@users.noreply.github.com>
2021-02-21 08:49:10 -0800

    2.1

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ import sqlite3
 
 
 def connect_db():
-    # sql = sqlite3.connect("./questions.db")
-    sql = sqlite3.connect("/Users/jahaan/OneDrive/Programming/Forum changes/forum/questions.db")
+    sql = sqlite3.connect("./questions.db")
+    # sql = sqlite3.connect("/Users/jahaan/OneDrive/Programming/Forum changes/forum/questions.db")
     print("True")
     sql.row_factory = sqlite3.Row
     return sql
